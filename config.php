ffbd002a1becd2242a2b7d2d39bfc337ee7b89fe
Divyya Sai Gorantla <123133078+DivyaGorantl@users.noreply.github.com>
2026-04-01 01:16:54 +0530

    Change database host from 'mysqldb2' to 'mysqldb'

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$host = "mysqldb2"; /* Host name */
+$host = "mysqldb"; /* Host name */
 $user = "root"; /* User */
 $password = "admin123"; /* Password */
 $dbname = "customers"; /* Database name */
